c9d39af6593c6c2be997051e0a6c69c6e2b6c2d1
Akash Kumar <88193959+Soraakash@users.noreply.github.com>
2023-03-30 11:09:41 +0530

    Day 2 (Medium had to do again

---
+++ Day 2/912 Sort An Array.cpp
@@ -0,0 +1,37 @@
+// Given an array of integers nums, sort the array in ascending order and return it.
+
+// You must solve the problem without using any built-in functions in O(nlog(n)) time complexity and with the smallest space complexity possible.
+
+// Example 1:
+// Input: nums = [5,2,3,1]
+// Output: [1,2,3,5]
+
+class Solution {
+public:
+    void merge(vector<int>& nums, int l, int m, int r){
+        vector<int> tmp(r - l + 1);
+        int i = l; // index for left subarray
+        int j = m + 1; // index for right subarray
+        int k = 0; // index for temporary array
+        while(i <= m && j <= r){
+            if(nums[i] <= nums[j]) tmp[k++] = nums[i++]; 
+            else tmp[k++] = nums[j++];
+        }
+        while(i <= m) tmp[k++] = nums[i++];
+        while(j <= r) tmp[k++] = nums[j++]; 
+        for(i = 0; i < k; i++) nums[l + i] = tmp[i];
+    }
+	
+	// mergeSort(nums, 0, nums.size() - 1);
+    void mergeSort(vector<int>& nums, int l, int r){
+        if(l >= r) return;
+        int m = l + (r - l) / 2; //middle index, same as (l+r)/2
+        mergeSort(nums, l, m);
+        mergeSort(nums, m + 1, r);
+        merge(nums, l, m, r);
+    }
+    vector<int> sortArray(vector<int>& nums) {
+        mergeSort(nums,0,nums.size()-1);
+        return nums;
+    }
+};
