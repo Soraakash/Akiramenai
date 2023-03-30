93d160955b9a37c1d9c166bb75285b224f889fdb
Akash Kumar <88193959+Soraakash@users.noreply.github.com>
2023-03-30 11:11:23 +0530

    Day 2 (Medium had to do again)

--- Day 2/912 Sort An Array.cpp
+++ Day 2/912 Sort An Array.cpp
@@ -21,6 +21,7 @@ public:
         while(j <= r) tmp[k++] = nums[j++]; 
         for(i = 0; i < k; i++) nums[l + i] = tmp[i];
     }
+	
 	// mergeSort(nums, 0, nums.size() - 1);
     void mergeSort(vector<int>& nums, int l, int r){
         if(l >= r) return;
