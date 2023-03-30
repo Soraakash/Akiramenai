aace95051fef4325cfcfe06a953997c0230e7bdd
Akash Kumar <88193959+Soraakash@users.noreply.github.com>
2023-03-30 06:39:50 +0530

    Day 2 (Medium had to do again)

---
+++ Day 2/2012 Sum of Beauty in the Array.cpp
@@ -0,0 +1,40 @@
+// You are given a 0-indexed integer array nums. For each index i (1 <= i <= nums.length - 2) the beauty of nums[i] equals:
+
+// 2, if nums[j] < nums[i] < nums[k], for all 0 <= j < i and for all i < k <= nums.length - 1.
+// 1, if nums[i - 1] < nums[i] < nums[i + 1], and the previous condition is not satisfied.
+// 0, if none of the previous conditions holds.
+// Return the sum of beauty of all nums[i] where 1 <= i <= nums.length - 2.
+
+// Example 1:
+
+// Input: nums = [1,2,3]
+// Output: 2
+// Explanation: For each index i in the range 1 <= i <= 1:
+// - The beauty of nums[1] equals 2.
+
+class Solution {
+public:
+    int sumOfBeauties(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> prefix(n+1),suffix(n+1);
+        prefix[0]=nums[0];
+        suffix[n-1]=nums[n-1];
+        int sum=0;
+        for(int i=1;i<nums.size();i++)
+        {
+            prefix[i]=max(prefix[i-1],nums[i]);
+        }
+         for(int i=n-2;i>=0;i--)
+        {
+            suffix[i]=min(suffix[i+1],nums[i]);
+        }
+        for(int i=1;i<nums.size()-1;i++)
+        {
+            if(nums[i]>prefix[i-1]&&nums[i]<suffix[i+1])
+            sum+=2;
+            else if (nums[i-1]<nums[i]&&nums[i]<nums[i+1])
+            sum+=1;
+        }
+        return sum; 
+    }
+};
