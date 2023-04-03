438b43d886f29816b631f7f8c994e07e2c6bc739
Akash Kumar <88193959+Soraakash@users.noreply.github.com>
2023-04-03 14:33:24 +0530

    Day 6 (Easy)

---
+++ Day 6/2248 Intersection of Multiple Arrays.cpp
@@ -0,0 +1,28 @@
+// Given a 2D integer array nums where nums[i] is a non-empty array of distinct positive integers,
+// return the list of integers that are present in each array of nums sorted in ascending order.
+ 
+// Example 1:
+// Input: nums = [[3,1,2,4,5],[1,2,3,4],[3,4,5,6]]
+// Output: [3,4]
+// Explanation: 
+// The only integers present in each of nums[0] = [3,1,2,4,5], nums[1] = [1,2,3,4], and nums[2] = [3,4,5,6] are 3 and 4, so we return [3,4].
+
+class Solution {
+public:
+    vector<int> intersection(vector<vector<int>>& nums) {
+        int n =nums.size();
+        map<int,int>mp;
+        for(int i=0;i<nums.size();i++)
+        {
+            for(int j=0;j<nums[i].size();j++)
+                mp[nums[i][j]]++;
+        }
+        vector<int> v;
+        for(auto it:mp)
+        {
+            if(it.second == n)
+                v.push_back(it.first);
+        }
+        return v;
+    }
+};
